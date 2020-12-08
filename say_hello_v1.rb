f93b324bcf5c543b0efdcd9801138aa6463aea01
Michael Wang <michwng@gmail.com>
2020-12-08 22:51:50 +0000

    Automatically backed up by Learn

--- say_hello_v1.rb
+++ say_hello_v1.rb
@@ -1,8 +1,14 @@
 # Build your say_hello method here
 
-def say_hello(name = "Ruby Programmer")
+def say_hello
+  puts "What is your name?"
+  name = gets 
+  puts "Hello #{name = "Ruby Programmer"}."
+end
+  
+  
+  def say_hello
   puts "What is your name?"
   name = gets 
   puts "Hello #{name}."
 end
-  
